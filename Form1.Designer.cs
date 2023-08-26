ef0fca5e551cdb6120ca0a3b1ed0610281db60af
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-08-26 22:44:56 +0530

    user reg ok

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
+            registerBtn = new Button();
+            fullName = new TextBox();
+            label1 = new Label();
+            fullNameLabel = new Label();
+            emailLabel = new Label();
+            email = new TextBox();
+            positionLabel = new Label();
+            position = new TextBox();
+            passwordLabel = new Label();
+            password = new TextBox();
+            confirmPasswordLabel = new Label();
+            confirmPassword = new TextBox();
             SuspendLayout();
+            // 
+            // registerBtn
+            // 
+            registerBtn.BackgroundImageLayout = ImageLayout.None;
+            registerBtn.Cursor = Cursors.Hand;
+            registerBtn.FlatAppearance.BorderSize = 2;
+            registerBtn.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            registerBtn.Location = new Point(37, 434);
+            registerBtn.Name = "registerBtn";
+            registerBtn.Size = new Size(218, 42);
+            registerBtn.TabIndex = 0;
+            registerBtn.Text = "Register";
+            registerBtn.UseVisualStyleBackColor = true;
+            registerBtn.Click += button1_Click;
+            // 
+            // fullName
+            // 
+            fullName.BorderStyle = BorderStyle.FixedSingle;
+            fullName.Location = new Point(37, 67);
+            fullName.Margin = new Padding(6);
+            fullName.MinimumSize = new Size(30, 30);
+            fullName.Name = "fullName";
+            fullName.Size = new Size(218, 30);
+            fullName.TabIndex = 1;
+            fullName.TextChanged += fullName_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(160, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 2;
+            // 
+            // fullNameLabel
+            // 
+            fullNameLabel.AutoSize = true;
+            fullNameLabel.Location = new Point(37, 49);
+            fullNameLabel.Name = "fullNameLabel";
+            fullNameLabel.Size = new Size(61, 15);
+            fullNameLabel.TabIndex = 3;
+            fullNameLabel.Text = "Full Name";
+            fullNameLabel.Click += label2_Click;
+            // 
+            // emailLabel
+            // 
+            emailLabel.AutoSize = true;
+            emailLabel.Location = new Point(37, 115);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new Size(36, 15);
+            emailLabel.TabIndex = 5;
+            emailLabel.Text = "Email";
+            emailLabel.Click += label3_Click;
+            // 
+            // email
+            // 
+            email.BorderStyle = BorderStyle.FixedSingle;
+            email.Location = new Point(37, 133);
+            email.Margin = new Padding(6);
+            email.MinimumSize = new Size(30, 30);
+            email.Name = "email";
+            email.Size = new Size(218, 30);
+            email.TabIndex = 4;
+            email.TextChanged += textBox1_TextChanged;
+            // 
+            // positionLabel
+            // 
+            positionLabel.AutoSize = true;
+            positionLabel.Location = new Point(37, 192);
+            positionLabel.Name = "positionLabel";
+            positionLabel.Size = new Size(50, 15);
+            positionLabel.TabIndex = 7;
+            positionLabel.Text = "Position";
+            positionLabel.Click += label2_Click_1;
+            // 
+            // position
+            // 
+            position.BorderStyle = BorderStyle.FixedSingle;
+            position.Location = new Point(37, 210);
+            position.Margin = new Padding(6);
+            position.MinimumSize = new Size(30, 30);
+            position.Name = "position";
+            position.Size = new Size(218, 30);
+            position.TabIndex = 6;
+            position.TextChanged += position_TextChanged;
+            // 
+            // passwordLabel
+            // 
+            passwordLabel.AutoSize = true;
+            passwordLabel.Location = new Point(37, 269);
+            passwordLabel.Name = "passwordLabel";
+            passwordLabel.Size = new Size(57, 15);
+            passwordLabel.TabIndex = 9;
+            passwordLabel.Text = "Password";
+            passwordLabel.Click += label2_Click_2;
+            // 
+            // password
+            // 
+            password.BorderStyle = BorderStyle.FixedSingle;
+            password.Location = new Point(37, 287);
+            password.Margin = new Padding(6);
+            password.MinimumSize = new Size(30, 30);
+            password.Name = "password";
+            password.Size = new Size(218, 30);
+            password.TabIndex = 8;
+            password.TextChanged += password_TextChanged;
+            // 
+            // confirmPasswordLabel
+            // 
+            confirmPasswordLabel.AutoSize = true;
+            confirmPasswordLabel.Location = new Point(37, 338);
+            confirmPasswordLabel.Name = "confirmPasswordLabel";
+            confirmPasswordLabel.Size = new Size(103, 15);
+            confirmPasswordLabel.TabIndex = 11;
+            confirmPasswordLabel.Text = "Confirm Password";
+            // 
+            // confirmPassword
+            // 
+            confirmPassword.BorderStyle = BorderStyle.FixedSingle;
+            confirmPassword.Location = new Point(37, 356);
+            confirmPassword.Margin = new Padding(6);
+            confirmPassword.MinimumSize = new Size(30, 30);
+            confirmPassword.Name = "confirmPassword";
+            confirmPassword.Size = new Size(218, 30);
+            confirmPassword.TabIndex = 10;
+            confirmPassword.TextChanged += confirmPassword_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(876, 506);
+            ClientSize = new Size(976, 588);
+            Controls.Add(confirmPasswordLabel);
+            Controls.Add(confirmPassword);
+            Controls.Add(passwordLabel);
+            Controls.Add(password);
+            Controls.Add(positionLabel);
+            Controls.Add(position);
+            Controls.Add(emailLabel);
+            Controls.Add(email);
+            Controls.Add(fullNameLabel);
+            Controls.Add(label1);
+            Controls.Add(fullName);
+            Controls.Add(registerBtn);
+            Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Register";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button registerBtn;
+        private TextBox fullName;
+        private Label label1;
+        private Label fullNameLabel;
+        private Label emailLabel;
+        private TextBox email;
+        private Label positionLabel;
+        private TextBox position;
+        private Label passwordLabel;
+        private TextBox password;
+        private Label confirmPasswordLabel;
+        private TextBox confirmPassword;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,26 +2,143 @@ using MySql.Data.MySqlClient;
 namespace HealthCarePlus
 {
     public partial class Form1 : Form
+         
     {
+        string mysqlCon = "server='127.0.0.1'; user=root; database=hospital; password= ";
         public Form1()
         {
             InitializeComponent();
-            string mysqlCon = "server='127.0.0.1'; user=root; database=hospital; password= ";
-            MySqlConnection mySqlConnection = new MySqlConnection (mysqlCon);
-            try
-            {
-                mySqlConnection.Open ();
-                MessageBox.Show("Connection success");
-            }catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally { mySqlConnection.Close (); }
+            //MySqlConnection mySqlConnection = new MySqlConnection(mysqlCon);
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                RegisterUser();
+            }
+
+        }
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(fullName.Text) ||
+                string.IsNullOrWhiteSpace(email.Text) ||
+                string.IsNullOrWhiteSpace(position.Text) ||
+                string.IsNullOrWhiteSpace(password.Text) ||
+                string.IsNullOrWhiteSpace(confirmPassword.Text))
+            {
+                MessageBox.Show("All fields are required.");
+                return false;
+            }
+
+            if (!IsValidEmail(email.Text))
+            {
+                MessageBox.Show("Invalid email address.");
+                return false;
+            }
+
+            if (password.Text != confirmPassword.Text)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            return System.Text.RegularExpressions.Regex.IsMatch(email, pattern);
+        }
+
+        private void RegisterUser()
+        {
+            using (MySqlConnection mySqlConnection = new MySqlConnection(mysqlCon))
+            {
+                try
+                {
+                    mySqlConnection.Open();
+                    string insertQuery = "INSERT INTO userstable (FullName, Email, Position, Password) VALUES (@FullName, @Email, @Position, @Password)";
+                    MySqlCommand cmd = new MySqlCommand(insertQuery, mySqlConnection);
+                    cmd.Parameters.AddWithValue("@FullName", fullName.Text);
+                    cmd.Parameters.AddWithValue("@Email", email.Text);
+                    cmd.Parameters.AddWithValue("@Position", position.Text);
+                    cmd.Parameters.AddWithValue("@Password", password.Text);
+                    int result = cmd.ExecuteNonQuery();
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Registration successful.");
+                        ClearInputs();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Registration failed.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void ClearInputs()
+        {
+            fullName.Text = "";
+            email.Text = "";
+            position.Text = "";
+            password.Text = "";
+            confirmPassword.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //email
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void fullName_TextChanged(object sender, EventArgs e)
+        {
+            //full name
+        }
+
+        private void position_TextChanged(object sender, EventArgs e)
+        {
+            //possition
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            //password
+        }
+
+        private void confirmPassword_TextChanged(object sender, EventArgs e)
+        {
+            //confirm pass
         }
     }
 }
