ab4e1c99259fb19d650c98134f7ae844ad9383e4
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-09-07 01:40:41 +0530

    admin dashboard re change

--- AdminDashboard.cs
+++ AdminDashboard.cs
@@ -1,6 +1,4 @@
-﻿using MaterialSkin;
-using MaterialSkin.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,28 +10,11 @@ using System.Windows.Forms;
 
 namespace HealthCarePlus
 {
-    public partial class AdminDashboard : MaterialForm
+    public partial class AdminDashboard : Form
     {
-        readonly MaterialSkin.MaterialSkinManager materialSkinManager;
         public AdminDashboard()
         {
             InitializeComponent();
-            materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
-            materialSkinManager.EnforceBackcolorOnAllComponents = true;
-            materialSkinManager.AddFormToManage(this);
-            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Indigo600, MaterialSkin.Primary.Indigo700, MaterialSkin.Primary.Indigo100, MaterialSkin.Accent.Pink200, MaterialSkin.TextShade.WHITE);
-
-        }
-
-        private void Form4_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
