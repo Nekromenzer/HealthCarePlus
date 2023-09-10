8f7661056acb2719f8551ccc32ab580a26c6f675
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-09-10 19:10:34 +0530

    bind table

--- DoctorSearch.Designer.cs
+++ DoctorSearch.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace HealthCarePlus
+﻿using System.Windows.Forms;
+
+namespace HealthCarePlus
 {
     partial class searchForm
     {
@@ -28,21 +30,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(searchForm));
+            doctorTable = new DataGridView();
+            doctorId = new DataGridViewTextBoxColumn();
+            doctorFullName = new DataGridViewTextBoxColumn();
+            doctorContactNumber = new DataGridViewTextBoxColumn();
+            doctorEmail = new DataGridViewTextBoxColumn();
+            doctorLocation = new DataGridViewTextBoxColumn();
+            doctorAvailability = new DataGridViewTextBoxColumn();
+            doctorExpertise = new DataGridViewTextBoxColumn();
+            doctorOtherDetails = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)doctorTable).BeginInit();
             SuspendLayout();
+            // 
+            // doctorTable
+            // 
+            doctorTable.AllowUserToAddRows = false;
+            doctorTable.AllowUserToOrderColumns = true;
+            doctorTable.AllowUserToResizeColumns = false;
+            doctorTable.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            doctorTable.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            doctorTable.BackgroundColor = SystemColors.Control;
+            doctorTable.BorderStyle = BorderStyle.None;
+            doctorTable.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            doctorTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            doctorTable.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            doctorTable.Columns.AddRange(new DataGridViewColumn[] { doctorId, doctorFullName, doctorContactNumber, doctorEmail, doctorLocation, doctorAvailability, doctorExpertise, doctorOtherDetails });
+            doctorTable.EnableHeadersVisualStyles = false;
+            doctorTable.Location = new Point(68, 304);
+            doctorTable.Name = "doctorTable";
+            doctorTable.RowHeadersVisible = false;
+            doctorTable.RowTemplate.Height = 25;
+            doctorTable.ScrollBars = ScrollBars.Vertical;
+            doctorTable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            doctorTable.Size = new Size(1193, 484);
+            doctorTable.TabIndex = 0;
+            doctorTable.CellContentClick += doctorTable_CellContentClick;
+            // 
+            // doctorId
+            // 
+            doctorId.DataPropertyName = "DoctorID";
+            doctorId.HeaderText = "Id";
+            doctorId.Name = "doctorId";
+            doctorId.Width = 41;
+            // 
+            // doctorFullName
+            // 
+            doctorFullName.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            doctorFullName.DataPropertyName = "FullName";
+            doctorFullName.HeaderText = "Full Name";
+            doctorFullName.Name = "doctorFullName";
+            // 
+            // doctorContactNumber
+            // 
+            doctorContactNumber.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            doctorContactNumber.DataPropertyName = "ContactNumber";
+            doctorContactNumber.HeaderText = "Contact";
+            doctorContactNumber.Name = "doctorContactNumber";
+            doctorContactNumber.Width = 73;
+            // 
+            // doctorEmail
+            // 
+            doctorEmail.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            doctorEmail.DataPropertyName = "Email";
+            doctorEmail.HeaderText = "Email";
+            doctorEmail.Name = "doctorEmail";
+            // 
+            // doctorLocation
+            // 
+            doctorLocation.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            doctorLocation.DataPropertyName = "Location";
+            doctorLocation.HeaderText = "Location";
+            doctorLocation.Name = "doctorLocation";
+            // 
+            // doctorAvailability
+            // 
+            doctorAvailability.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            doctorAvailability.DataPropertyName = "Availability";
+            doctorAvailability.HeaderText = "Availability";
+            doctorAvailability.Name = "doctorAvailability";
+            // 
+            // doctorExpertise
+            // 
+            doctorExpertise.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            doctorExpertise.DataPropertyName = "Expertise";
+            doctorExpertise.HeaderText = "Expertise";
+            doctorExpertise.Name = "doctorExpertise";
+            // 
+            // doctorOtherDetails
+            // 
+            doctorOtherDetails.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            doctorOtherDetails.DataPropertyName = "OtherDetails";
+            doctorOtherDetails.HeaderText = "Other Details";
+            doctorOtherDetails.Name = "doctorOtherDetails";
             // 
             // searchForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1325, 834);
+            Controls.Add(doctorTable);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "searchForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Search";
+            ((System.ComponentModel.ISupportInitialize)doctorTable).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView doctorTable;
+        private DataGridViewTextBoxColumn doctorId;
+        private DataGridViewTextBoxColumn doctorFullName;
+        private DataGridViewTextBoxColumn doctorContactNumber;
+        private DataGridViewTextBoxColumn doctorEmail;
+        private DataGridViewTextBoxColumn doctorLocation;
+        private DataGridViewTextBoxColumn doctorAvailability;
+        private DataGridViewTextBoxColumn doctorExpertise;
+        private DataGridViewTextBoxColumn doctorOtherDetails;
     }
 }
--- DoctorSearch.cs
+++ DoctorSearch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,35 @@ namespace HealthCarePlus
 {
     public partial class searchForm : Form
     {
+        private string mysqlCon = "Data source=127.0.0.1; user=root; database=hospital; password= ";
         public searchForm()
         {
             InitializeComponent();
+            DisplayDoctorList();
+        }
+
+        private void DisplayDoctorList()
+        {
+            using (MySqlConnection conn = new MySqlConnection(mysqlCon))
+            {
+                conn.Open();
+                string query = "SELECT * FROM doctors";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    // bind table
+                    doctorTable.DataSource = dataTable;
+                }
+            }
+        }
+
+
+        private void doctorTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
