fea04de37a95b6bfc68b1b3b3fda4b978ca44a7f
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-09-11 13:36:55 +0530

    bind table data

--- PatientAdd.Designer.cs
+++ PatientAdd.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatientAddForm));
             patientTable = new DataGridView();
+            id = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            dob = new DataGridViewTextBoxColumn();
+            contact = new DataGridViewTextBoxColumn();
+            address = new DataGridViewTextBoxColumn();
+            nic = new DataGridViewTextBoxColumn();
+            gender = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)patientTable).BeginInit();
             SuspendLayout();
             // 
@@ -39,13 +47,92 @@
             patientTable.AllowUserToDeleteRows = false;
             patientTable.AllowUserToResizeColumns = false;
             patientTable.AllowUserToResizeRows = false;
+            patientTable.BackgroundColor = Color.WhiteSmoke;
+            patientTable.BorderStyle = BorderStyle.None;
+            patientTable.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.ActiveCaption;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = Color.IndianRed;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            patientTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             patientTable.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            patientTable.Columns.AddRange(new DataGridViewColumn[] { id, name, dob, contact, address, nic, gender });
             patientTable.Location = new Point(62, 303);
             patientTable.Name = "patientTable";
             patientTable.ReadOnly = true;
             patientTable.RowTemplate.Height = 25;
+            patientTable.ScrollBars = ScrollBars.Vertical;
+            patientTable.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            patientTable.ShowCellErrors = false;
+            patientTable.ShowCellToolTips = false;
+            patientTable.ShowEditingIcon = false;
+            patientTable.ShowRowErrors = false;
             patientTable.Size = new Size(1193, 484);
             patientTable.TabIndex = 0;
+            // 
+            // id
+            // 
+            id.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
+            id.DataPropertyName = "PatientID";
+            id.HeaderText = "ID";
+            id.Name = "id";
+            id.ReadOnly = true;
+            id.Width = 43;
+            // 
+            // name
+            // 
+            name.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            name.DataPropertyName = "FullName";
+            name.HeaderText = "Full Name";
+            name.Name = "name";
+            name.ReadOnly = true;
+            // 
+            // dob
+            // 
+            dob.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dob.DataPropertyName = "DateOfBirth";
+            dob.HeaderText = "DOB";
+            dob.Name = "dob";
+            dob.ReadOnly = true;
+            dob.Width = 56;
+            // 
+            // contact
+            // 
+            contact.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            contact.DataPropertyName = "ContactNumber";
+            contact.HeaderText = "Contact";
+            contact.Name = "contact";
+            contact.ReadOnly = true;
+            contact.Width = 74;
+            // 
+            // address
+            // 
+            address.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            address.DataPropertyName = "Address";
+            address.HeaderText = "Address";
+            address.Name = "address";
+            address.ReadOnly = true;
+            // 
+            // nic
+            // 
+            nic.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            nic.DataPropertyName = "NIC";
+            nic.HeaderText = "NIC";
+            nic.Name = "nic";
+            nic.ReadOnly = true;
+            nic.Width = 52;
+            // 
+            // gender
+            // 
+            gender.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            gender.DataPropertyName = "Gender";
+            gender.HeaderText = "Gender";
+            gender.Name = "gender";
+            gender.ReadOnly = true;
+            gender.Width = 70;
             // 
             // PatientAddForm
             // 
@@ -58,6 +145,7 @@
             Name = "PatientAddForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Patient Add";
+            Load += PatientAddForm_Load;
             ((System.ComponentModel.ISupportInitialize)patientTable).EndInit();
             ResumeLayout(false);
         }
@@ -65,5 +153,12 @@
         #endregion
 
         private DataGridView patientTable;
+        private DataGridViewTextBoxColumn id;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn dob;
+        private DataGridViewTextBoxColumn contact;
+        private DataGridViewTextBoxColumn address;
+        private DataGridViewTextBoxColumn nic;
+        private DataGridViewTextBoxColumn gender;
     }
 }
--- PatientAdd.cs
+++ PatientAdd.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,36 @@ using System.Windows.Forms;
 
 namespace HealthCarePlus
 {
+   
     public partial class PatientAddForm : Form
     {
+        private string mysqlCon = "Data source=127.0.0.1; user=root; database=hospital; password= ";
         public PatientAddForm()
         {
             InitializeComponent();
+        }
+
+        private void DisplayDoctorList()
+        {
+            using (MySqlConnection conn = new MySqlConnection(mysqlCon))
+            {
+                conn.Open();
+                string query = "SELECT * FROM patients";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    // bind table
+                    patientTable.DataSource = dataTable;
+                }
+            }
+        }
+
+        private void PatientAddForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
